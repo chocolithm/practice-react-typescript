e100dc62b6fd1b7146145d36b1ddcfc81e39bb59
chocolithm <kys8336@gmail.com>
2025-03-21 20:58:49 +0900

    리액트 동작 원리

---
+++ ch02/ch02_3/src/App.tsx
@@ -0,0 +1,9 @@
+import ArrowComponent from './ArrowComponent';
+
+export default function App() {
+  return (
+    <ul>
+      <ArrowComponent href="https://twitter.com" text="go to Twitter" />
+    </ul>
+  );
+}
