3a332b9229deff6dd359148833848b2687ebf85f
chocolithm <kys8336@gmail.com>
2025-03-26 00:05:37 +0900

    tailwindcss 2

--- ch03/ch03_3/src/components/Div.tsx
+++ ch03/ch03_3/src/components/Div.tsx
@@ -1,12 +1,14 @@
 import type { FC, DetailedHTMLProps, HTMLAttributes, PropsWithChildren } from 'react';
 import type { WidthHeight } from './WidthHeight';
+import type { LeftRightTopBottom } from './LeftRightTopBottom';
 
 export type ReactDivProps = DetailedHTMLProps<
   HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
 >;
 export type DivProps = ReactDivProps &
-  PropsWithChildren<WidthHeight> & {
+  PropsWithChildren<WidthHeight> &
+  LeftRightTopBottom & {
     src?: string;
   };
 
@@ -16,9 +18,22 @@ export const Div: FC<DivProps> = ({
   style: _style,
   src,
   className: _className,
+  left,
+  right,
+  top,
+  bottom,
   ...props
 }) => {
-  const style = { ..._style, width, height, backgroundImage: src && `url(${src})` };
+  const style = {
+    ..._style,
+    width,
+    height,
+    backgroundImage: src && `url(${src})`,
+    left,
+    right,
+    top,
+    bottom
+  };
   const className = ['box-border', src && 'bg-gray-300', _className].join(' ');
   return <div {...props} className={className} style={style} />;
 };
