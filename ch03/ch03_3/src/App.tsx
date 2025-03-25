aa2c431856aec17aabcaa782ecbeba3aa745e2c5
chocolithm <kys8336@gmail.com>
2025-03-25 23:43:08 +0900

    tailwindcss 1

---
+++ ch03/ch03_3/src/App.tsx
@@ -0,0 +1,33 @@
+import DivTest from "./pages/DivTest";
+import ViewportTest from "./pages/ViewportTest";
+import HeightTest from "./pages/HeightTest";
+import PaddingTest from "./pages/PaddingTest";
+import MarginTest from "./pages/MarginTest";
+import ImageTest from "./pages/ImageTest";
+import BackgroundTest from "./pages/BackgroundTest";
+import DisplayTest from "./pages/DisplayTest";
+import DisplayNoneTest from "./pages/DisplayNoneTest";
+import AvatarTest from "./pages/AvatarTest";
+import BorderTest from "./pages/BorderTest";
+import PositionTest from "./pages/PositionTest";
+import OverlayTest from "./pages/OverlayTest";
+
+export default function App() {
+  return (
+    <main>
+      <DivTest />
+      <ViewportTest />
+      <HeightTest />
+      <PaddingTest />
+      <MarginTest />
+      <ImageTest />
+      <BackgroundTest />
+      <DisplayTest />
+      <DisplayNoneTest />
+      <AvatarTest />
+      <BorderTest />
+      <PositionTest />
+      <OverlayTest />
+    </main>
+  );
+}
