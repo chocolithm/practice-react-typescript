da6e6505cf510c8c0d6ef5ff4633790c54574084
chocolithm <kys8336@gmail.com>
2025-03-26 22:50:26 +0900

    useEffect

---
+++ ch04/ch04_4/src/copy/CopyMe.tsx
@@ -0,0 +1,10 @@
+import { Title } from '../components';
+
+export default function CopyMe() {
+  return (
+    <section className="mt-4">
+      <Title></Title>
+      <div className="flex flex-col items-center mt-4"></div>
+    </section>
+  );
+}
