347a88887622350f6dad86fa5edd036d1cf41d30
chocolithm <kys8336@gmail.com>
2025-04-05 13:00:23 +0900

    trello 만들기 - 리스트 및 카드 이동

--- ch05/ch05_4/src/store/useLists.ts
+++ ch05/ch05_4/src/store/useLists.ts
@@ -1,3 +1,4 @@
+import type { DropResult } from 'react-beautiful-dnd';
 import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import type { AppState } from './AppState';
@@ -6,6 +7,7 @@ import * as LO from './listidOrders';
 import * as L from './listEntities';
 import * as C from './cardEntities';
 import * as LC from './listidCardidOrders';
+import * as U from '../utils';
 
 export const useLists = () => {
   const dispatch = useDispatch();
@@ -16,6 +18,10 @@ export const useLists = () => {
 
   const listidCardidOrders = useSelector<AppState, LC.State>(
     ({ listidCardidOrders }) => listidCardidOrders
+  );
+
+  const listidOrders = useSelector<AppState, LO.State>(
+    ({ listidOrders }) => listidOrders
   );
 
   const onCreateList = useCallback(
@@ -41,5 +47,66 @@ export const useLists = () => {
     [dispatch, listidCardidOrders]
   );
 
-  return { lists, onCreateList, onRemoveList };
+  const onMoveList = useCallback(
+    (dragIndex: number, hoverIndex: number) => {
+      const newOrders = listidOrders.map((item, index) =>
+        index === dragIndex
+          ? listidOrders[hoverIndex]
+          : index === hoverIndex
+          ? listidOrders[dragIndex]
+          : item
+      );
+      dispatch(LO.setListidOrders(newOrders));
+    },
+    [dispatch, listidOrders]
+  );
+
+  const onDragEnd = useCallback(
+    (result: DropResult) => {
+      console.log('onDragEnd result', result);
+      const destinationListid = result.destination?.droppableId;
+      const destinationCardIndex = result.destination?.index;
+      if (destinationListid === undefined || destinationCardIndex === undefined) return;
+
+      const sourceListid = result.source.droppableId;
+      const sourceCardIndex = result.source.index;
+
+      if (destinationListid === sourceListid) {
+        const cardidOrders = listidCardidOrders[destinationListid];
+
+        dispatch(
+          LC.setListidCardids({
+            listid: destinationListid,
+            cardids: U.swapItemsInArray(
+              cardidOrders,
+              sourceCardIndex,
+              destinationCardIndex
+            )
+          })
+        );
+      } else {
+        const sourceCardidOrders = listidCardidOrders[sourceListid];
+        dispatch(
+          LC.setListidCardids({
+            listid: sourceListid,
+            cardids: U.removeItemAtIndexInArray(sourceCardidOrders, sourceCardIndex)
+          })
+        );
+        const destinationCardidOrders = listidCardidOrders[destinationListid];
+        dispatch(
+          LC.setListidCardids({
+            listid: destinationListid,
+            cardids: U.insertItemAtIndexInArray(
+              destinationCardidOrders,
+              destinationCardIndex,
+              result.draggableId
+            )
+          })
+        );
+      }
+    },
+    [listidCardidOrders, dispatch]
+  );
+
+  return { lists, onCreateList, onRemoveList, onMoveList, onDragEnd };
 };
