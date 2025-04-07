9c240230df426f6858801cc11db53fa81eb6e735
chocolithm <kys8336@gmail.com>
2025-04-07 13:17:45 +0900

    Outlet, useResolvedPath, useMatch

---
+++ ch06/ch06_2/src/copy/CopyMe.tsx
@@ -0,0 +1,10 @@
+import { Title } from '../components';
+
+export default function CopyMe() {
+  return (
+    <section className="mt-4">
+      <Title></Title>
+      <div className="flex justify-center mt-4"></div>
+    </section>
+  );
+}
