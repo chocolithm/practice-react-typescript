eee443d0bc52c4823948023d6a6b216233aae932
chocolithm <kys8336@gmail.com>
2025-04-09 14:26:21 +0900

    client server 회원 연동 테스트

---
+++ ch07/ch07_4_client/src/copy/CopyMe/index.tsx
@@ -0,0 +1,8 @@
+export default function CopyMe() {
+  return (
+    <section className="mt-4">
+      <h2 className="flex justify-center mt-4">Copy Me</h2>
+      <div className="mt-4"></div>
+    </section>
+  );
+}
